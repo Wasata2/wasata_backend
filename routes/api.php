9d83990d9f444ead87e2e4c7ee0ea0b2d6bae034
ayafrena44-blip <ayafrena44@gmail.com>
2026-09-22 12:37:12 +0300

    update

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/auth/logout', [AuthController::class, 'logout']);
     Route::get('/auth/me',      [AuthController::class, 'me']);
     Route::put('/auth/profile', [AuthController::class, 'updateProfile']);
+    Route::match(['put', 'post'], '/auth/profile', [AuthController::class, 'updateProfile']);
 
     Route::get('/stores', [StoreController::class, 'browse']);
     Route::post('/stores', [StoreController::class, 'store']);
